a5a4f0c7d33cd46f1308332c7791781a913224cc
Pavelbour <pavelbour@gmail.com>
2021-12-24 14:45:13 +0300

    number button listener

--- app/src/main/java/ru/gb/androidcoursecalculator/MainActivity.java
+++ app/src/main/java/ru/gb/androidcoursecalculator/MainActivity.java
@@ -1,14 +1,27 @@
 package ru.gb.androidcoursecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display = findViewById(R.id.display);
+    }
+
+    public void numberButtonOnClick(View view) {
+        Button button = (Button) view;
+        if (display.getText().toString().equals("0")) {
+            display.setText("");
+        }
+        display.setText(display.getText().toString() + button.getText().toString());
     }
 }
